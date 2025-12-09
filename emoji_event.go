3010688be84b5a15c14405b53d4d3f1d79b9ec06
Hunter Horsman <kariudo@gmail.com>
2025-12-09 21:52:04 +0000

    feat: enhance emoji event handling with FlexibleTime and work item support

--- emoji_event.go
+++ emoji_event.go
@@ -1,8 +1,52 @@
 package gitlabwebhook
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
+
+	gitlab "gitlab.com/gitlab-org/api/client-go"
 )
+
+// FlexibleTime is a custom time type that can unmarshal from multiple time formats
+type FlexibleTime struct {
+	time.Time
+}
+
+// UnmarshalJSON implements json.Unmarshaler to handle multiple time formats
+func (ft *FlexibleTime) UnmarshalJSON(b []byte) error {
+	// Remove quotes from the JSON string
+	s := string(b)
+	if len(s) < 2 {
+		return fmt.Errorf("invalid time value: %s", s)
+	}
+	s = s[1 : len(s)-1] // Remove surrounding quotes
+
+	// Try multiple time formats
+	formats := []string{
+		time.RFC3339,                  // "2006-01-02T15:04:05Z07:00"
+		time.RFC3339Nano,              // "2006-01-02T15:04:05.999999999Z07:00"
+		"2006-01-02 15:04:05 MST",     // "2025-12-09 20:44:22 UTC"
+		"2006-01-02 15:04:05.999 MST", // With milliseconds
+	}
+
+	var lastErr error
+	for _, format := range formats {
+		t, err := time.Parse(format, s)
+		if err == nil {
+			ft.Time = t
+			return nil
+		}
+		lastErr = err
+	}
+
+	return fmt.Errorf("unable to parse time %q: %w", s, lastErr)
+}
+
+// MarshalJSON implements json.Marshaler
+func (ft FlexibleTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(ft.Time.Format(time.RFC3339))
+}
 
 // The gitlab client library (gitlab.com/gitlab-org/api/client-go) does not currently have a struct for EmojiEvent,
 // so we define it here.
@@ -15,6 +59,10 @@ type EmojiEvent struct {
 	ProjectID  int              `json:"project_id"`
 	Project    *EmojiProject    `json:"project"`
 	ObjectAttr *EmojiAttributes `json:"object_attributes"`
+	// These used to include an "Issue" but now include a "WorkItem"
+	WorkItem     *WorkItem            `json:"work_item,omitempty"`
+	Note         *gitlab.Note         `json:"note,omitempty"`
+	MergeRequest *gitlab.MergeRequest `json:"merge_request,omitempty"`
 }
 
 // EmojiUser represents the user who awarded the emoji
@@ -48,12 +96,51 @@ type EmojiProject struct {
 
 // EmojiAttributes represents the attributes of the emoji/award
 type EmojiAttributes struct {
-	UserID        int       `json:"user_id"`
-	CreatedAt     time.Time `json:"created_at"`
-	ID            int       `json:"id"`
-	Name          string    `json:"name"`
-	AwardableType string    `json:"awardable_type"`
-	AwardableID   int       `json:"awardable_id"`
-	UpdatedAt     time.Time `json:"updated_at"`
-	AwardedOnURL  string    `json:"awarded_on_url"`
+	UserID        int          `json:"user_id"`
+	CreatedAt     FlexibleTime `json:"created_at"`
+	ID            int          `json:"id"`
+	Name          string       `json:"name"`
+	AwardableType string       `json:"awardable_type"`
+	AwardableID   int          `json:"awardable_id"`
+	UpdatedAt     FlexibleTime `json:"updated_at"`
+	AwardedOnURL  string       `json:"awarded_on_url"`
+}
+
+// WorkItem represents a GitLab work item (issue)
+// These are not yet defined in the gitlab client library, so we define a
+// simplified version here as they seem a bit different than issues
+type WorkItem struct {
+	AuthorID         int           `json:"author_id"`
+	ClosedAt         *FlexibleTime `json:"closed_at"`
+	Confidential     bool          `json:"confidential"`
+	CreatedAt        FlexibleTime  `json:"created_at"`
+	Description      string        `json:"description"`
+	DiscussionLocked *bool         `json:"discussion_locked"`
+	DueDate          *string       `json:"due_date"`
+	ID               int           `json:"id"`
+	IID              int           `json:"iid"`
+	LastEditedAt     *FlexibleTime `json:"last_edited_at"`
+	LastEditedByID   *int          `json:"last_edited_by_id"`
+	MilestoneID      *int          `json:"milestone_id"`
+	MovedToID        *int          `json:"moved_to_id"`
+	DuplicatedToID   *int          `json:"duplicated_to_id"`
+	ProjectID        int           `json:"project_id"`
+	RelativePosition int           `json:"relative_position"`
+	StateID          int           `json:"state_id"`
+	TimeEstimate     int           `json:"time_estimate"`
+	Title            string        `json:"title"`
+	UpdatedAt        FlexibleTime  `json:"updated_at"`
+	UpdatedByID      int           `json:"updated_by_id"`
+	Weight           *int          `json:"weight"`
+	HealthStatus     *string       `json:"health_status"`
+	Type             string        `json:"type"`
+	URL              string        `json:"url"`
+	TotalTimeSpent   int           `json:"total_time_spent"`
+	TimeChange       int           `json:"time_change"`
+	AssigneeIDs      []int         `json:"assignee_ids"`
+	AssigneeID       int           `json:"assignee_id"`
+	Labels           []string      `json:"labels"`
+	State            string        `json:"state"`
+	Severity         string        `json:"severity"`
+	// ... add other fields as needed
 }
